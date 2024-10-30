779905232e68604b34786b608c7f02e5a58a13f7
manu0396 <manulucas0396@gmail.com>
2024-10-30 16:54:05 +0100

    Fix style

--- app/src/main/java/com/example/starwarsdocs/ui/components/CharacterItem.kt
+++ app/src/main/java/com/example/starwarsdocs/ui/components/CharacterItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -29,6 +30,7 @@ fun CharacterItem(navController: NavController, viewModel: SharedViewModel, char
             pressedElevation = 20.dp
         ),
         border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        shape = RoundedCornerShape(10),
         modifier = Modifier
             .padding(bottom = 16.dp, end = 16.dp)
             .fillMaxWidth()
