7ad1ed1711d79d9dfc75f302a88d04606bed84f9
manu0396 <manulucas0396@gmail.com>
2024-11-01 12:09:48 +0100

    Fix design detailTextScreen

--- app/src/main/java/com/example/starwarsdocs/ui/screens/DetailTextScreen.kt
+++ app/src/main/java/com/example/starwarsdocs/ui/screens/DetailTextScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +18,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -24,6 +30,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -54,37 +62,56 @@ fun SharedTransitionScope.DetailTextScreen(
         },
         contentWindowInsets = WindowInsets(16.dp)
     ) { contentPadding ->
-        Row(
+        Box (
+            contentAlignment = Alignment.Center,
             modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxSize(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-
+                .fillMaxSize()
         ) {
-            Text(
-                text = label,
+            Card(
+                elevation = CardDefaults.elevatedCardElevation(16.dp),
                 modifier = Modifier
-                    .sharedElement(
-                        state = rememberSharedContentState(key = "label/$label"),
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        boundsTransform = { _, _ ->
-                            tween(durationMillis = 1000)
-                        }
+                    .fillMaxSize(0.8f)
+                    .padding(top = 16.dp)
+                    .border(width = 1.dp, color = Color.Black, shape = RoundedCornerShape(10))
+                    .sharedBounds(
+                        sharedContentState = rememberSharedContentState(
+                            "bounds"
+                        ),
+                        animatedVisibilityScope = animatedVisibilityScope
+                    ),
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(contentPadding)
+                ) {
+                    Text(
+                        text = label,
+                        modifier = Modifier
+                            .sharedElement(
+                                state = rememberSharedContentState(key = "label/$label"),
+                                animatedVisibilityScope = animatedVisibilityScope,
+                                boundsTransform = { _, _ ->
+                                    tween(durationMillis = 1000)
+                                }
+                            )
                     )
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                text = text,
-                modifier = Modifier
-                    .sharedElement(
-                        state = rememberSharedContentState(key = "text/$text"),
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        boundsTransform = { _, _ ->
-                            tween(durationMillis = 1000)
-                        }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = text,
+                        modifier = Modifier
+                            .sharedElement(
+                                state = rememberSharedContentState(key = "text/$text"),
+                                animatedVisibilityScope = animatedVisibilityScope,
+                                boundsTransform = { _, _ ->
+                                    tween(durationMillis = 1000)
+                                }
+                            )
                     )
-            )
+                }
+            }
         }
     }
 }
